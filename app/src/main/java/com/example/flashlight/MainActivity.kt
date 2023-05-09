b0449da2515692af06f1c96f1f22865140a7632f
Ivana Belinić <belinicivana15@gmail.com>
2023-05-09 13:04:32 +0100

    Initial implementation of the flashlight toggle button #1

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.flashlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraManager: CameraManager
+    private var cameraId: String? = null
+    private lateinit var toggleButton: Button
+    private var isFlashlightOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        try {
+            cameraId =
+                cameraManager.cameraIdList[0] // Pretpostavljamo da prvi dostupni uređaj ima bljeskalicu
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+
+        toggleButton = findViewById(R.id.toggleButton)
+        toggleButton.setOnClickListener {
+            if (isFlashlightOn) {
+                turnOffFlashlight()
+            } else {
+                turnOnFlashlight()
+            }
+        }
+    }
+
+    private fun turnOnFlashlight() {
+        try {
+            cameraManager.setTorchMode(cameraId!!, true)
+            isFlashlightOn = true
+            toggleButton.text = getString(R.string.turn_off)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun turnOffFlashlight() {
+        try {
+            cameraManager.setTorchMode(cameraId!!, false)
+            isFlashlightOn = false
+            toggleButton.text = getString(R.string.turn_on)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
     }
 }
