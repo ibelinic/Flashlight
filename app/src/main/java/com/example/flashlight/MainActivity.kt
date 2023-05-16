050a9026bd7462aee04e35b3591f808bb67ee1a7
Ivana Belinić <belinicivana15@gmail.com>
2023-05-16 21:03:04 +0100

    Implemented visual effects of the application #6

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -8,6 +8,9 @@ import android.hardware.camera2.CameraManager
 import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
+import android.widget.Toast
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toggleButton: Button
     private var isFlashlightOn = false
     private lateinit var brightnessSeekBar: SeekBar
+    private var animation: ObjectAnimator? = null // Dodano polje za animaciju
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +28,12 @@ class MainActivity : AppCompatActivity() {
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         try {
-            cameraId =
-                cameraManager.cameraIdList[0]
+            cameraId = cameraManager.cameraIdList[0]
         } catch (e: CameraAccessException) {
             e.printStackTrace()
         }
 
         toggleButton = findViewById(R.id.toggleButton)
-        // Set the initial icon and text
         toggleButton.setCompoundDrawablesRelativeWithIntrinsicBounds(
             R.drawable.baseline_flashlight_on_24,
             0,
@@ -41,6 +43,9 @@ class MainActivity : AppCompatActivity() {
         toggleButton.text = getString(R.string.turn_on)
         brightnessSeekBar = findViewById(R.id.brightnessSeekBar)
 
+        val rootView = findViewById<View>(android.R.id.content)
+        rootView.setBackgroundColor(getColorForFlashlightStatus(isFlashlightOn))
+
         toggleButton.setOnClickListener {
             if (isFlashlightOn) {
                 turnOffFlashlight()
@@ -48,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 turnOnFlashlight()
             }
         }
+        toggleButton.setBackgroundColor(getButtonColorForFlashlightStatus(isFlashlightOn))
     }
 
     private fun turnOnFlashlight() {
@@ -63,6 +69,22 @@ class MainActivity : AppCompatActivity() {
             )
             brightnessSeekBar.visibility = View.VISIBLE
             brightnessSeekBar.progress = brightnessSeekBar.max
+            val rootView = findViewById<View>(android.R.id.content)
+            rootView.setBackgroundColor(getColorForFlashlightStatus(isFlashlightOn))
+            toggleButton.setBackgroundColor(getButtonColorForFlashlightStatus(isFlashlightOn))
+
+            // Create the button animation
+            animation = ObjectAnimator.ofPropertyValuesHolder(
+                toggleButton,
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 0.9f, 1f),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.9f, 1f)
+            )
+            animation?.duration = 100
+            animation?.repeatCount = ObjectAnimator.INFINITE
+            animation?.repeatMode = ObjectAnimator.REVERSE
+            animation?.start()
+
+            Toast.makeText(this@MainActivity, "Flashlight is turned ON", Toast.LENGTH_SHORT).show()
         } catch (e: CameraAccessException) {
             e.printStackTrace()
         }
@@ -74,14 +96,37 @@ class MainActivity : AppCompatActivity() {
             isFlashlightOn = false
             toggleButton.text = getString(R.string.turn_on)
             toggleButton.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                R.drawable.baseline_flashlight_on_24,
-                0,
+                R.drawable.baseline_flashlight_on_24,0,
                 0,
                 0
             )
             brightnessSeekBar.visibility = View.GONE
+            val rootView = findViewById<View>(android.R.id.content)
+            rootView.setBackgroundColor(getColorForFlashlightStatus(isFlashlightOn))
+            toggleButton.setBackgroundColor(getButtonColorForFlashlightStatus(isFlashlightOn))
+            // Cancel the button animation
+            animation?.cancel()
+            animation = null
+
+            Toast.makeText(this@MainActivity, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show()
         } catch (e: CameraAccessException) {
             e.printStackTrace()
+        }
+    }
+
+    private fun getColorForFlashlightStatus(isFlashlightOn: Boolean): Int {
+        return if (isFlashlightOn) {
+            getColor(R.color.white)
+        } else {
+            getColor(R.color.black)
+        }
+    }
+
+    private fun getButtonColorForFlashlightStatus(isFlashlightOn: Boolean): Int {
+        return if (isFlashlightOn) {
+            getColor(R.color.baby_blue)
+        } else {
+            getColor(R.color.yellow)
         }
     }
 }
